f9e514f98d3d17cd15492e214f03a13ef477d429
jelizasilatei <silateijc90@gmail.com>
2023-03-13 00:22:41 +0300

    Adding Event listenener

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+    let input = document.querySelector('input#button');
+
+    function clicked(){
+        console.log("I was clicked!");
+    }
+
+    input.addEventListener('click', clicked)
 }
+ addingEventListener();
+
